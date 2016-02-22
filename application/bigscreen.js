99bc926c3fa5eb4ef2b3b2cfde4558a6cf906815
Jouni Vepsäläinen <jovepsal@cs.helsinki.fi>
2016-02-22 08:58:57 +0200

    v1.0.9

--- application/bigscreen.js
+++ application/bigscreen.js
@@ -1,141 +1,86 @@
 #!/usr/bin/env node
 /**
  * BigScreen, 18.10.2013 Spaceify Inc.
- * 
+ *
+ * rpcObj = { is_secure: Boolean, id: Number, server_type: String, remotePort: Number, remoteAddress: Number, origin: String }
+ * serviceObj = { id: Number, is_secure: Boolean, service_name: String, server_type: String, remotePort: Number, remoteAddress: Number, origin: String }
+ *
  * @class BigScreen
  */
 
-var fs = require("fs");
-var fibrous = require("fibrous");
-var logger = require("/api/logger");
-var Config = require("/api/config")();
-var Utility = require("/api/utility");
-var Language = require("/api/language");
-var WebServer = require("/api/webserver");
-var WebSocketRPCClient = require("/api/websocketrpcclient");
-var WebSocketRPCServer = require("/api/websocketrpcserver");
+var spaceify = require("/api/spaceifyapplication.js");
 
 function BigScreen()
 {
-	var self = this;
+var self = this;
 
-	var bigscreens = {};
-	var httpServer = new WebServer();
-	var httpsServer = new WebServer();
-	var rpcCore = new WebSocketRPCClient();
-	var RPCS = new WebSocketRPCServer();
-	var RPCSS = new WebSocketRPCServer();
-	var www_path = Config.APPLICATION_WWW_PATH;
-	var ca_crt = Config.API_WWW_PATH + Config.SPACEIFY_CRT;
-	var key = Config.APPLICATION_TLS_PATH + Config.SERVER_KEY;
-	var crt = Config.APPLICATION_TLS_PATH + Config.SERVER_CRT;
+var bigscreens = {};
+var bs_service = bs_service_secure = null;
+var bs_service_name = "spaceify.org/services/bigscreen";
 
-	self.start = fibrous( function()
+	// CONNECTION LISTENERS -- -- -- -- -- -- -- -- -- -- //
+var onClientConnected = function(serviceObj)
 	{
-		try
-		{
-			// Establish a RPC connection to the Spaceify Core
-			rpcCore.sync.connect({hostname: Config.EDGE_HOSTNAME, port: Config.CORE_PORT_WEBSOCKET, persistent: true, owner: "bigscreen"});
-			//rpcCore.sync.connect({hostname: Config.EDGE_HOSTNAME, port: Config.CORE_PORT_WEBSOCKET_SECURE, is_secure: true, ca_crt: ca_crt, persistent: true, owner: "bigscreen"});
-
-			// Start applications JSON-RPC ws and wss servers
-			RPCS.exposeMethod("loadContent", self, self.loadContent);
-			RPCS.exposeMethod("getBigScreenIds", self, self.getBigScreenIds);
-			RPCS.exposeMethod("bigScreenConnect", self, self.bigScreenConnect);
-			RPCS.setDisconnectionListener(disconnectionListener);
-			RPCS.connect.sync({hostname: null, port: Config.FIRST_SERVICE_PORT, owner: "bigscreen"});
-
-			RPCSS.exposeMethod("loadContent", self, self.loadContent);
-			RPCSS.exposeMethod("getBigScreenIds", self, self.getBigScreenIds);
-			RPCSS.exposeMethod("bigScreenConnect", self, self.bigScreenConnect);
-			RPCSS.setDisconnectionListener(disconnectionListener);
-			RPCSS.connect.sync({hostname: null, port: Config.FIRST_SERVICE_PORT_SECURE, is_secure: true, key: key, crt: crt, ca_crt: ca_crt, owner: "bigscreen"});
-			
-			// Start applications http and https web servers
-			httpServer.connect.sync({hostname: null, port: 80, www_path: www_path, owner: "bigscreen"});
-			httpsServer.connect.sync({hostname: null, port: 443, is_secure: true, key: key, crt: crt, ca_crt: ca_crt, www_path: www_path, owner: "bigscreen"});
-
-			// Register provided services
-			rpcCore.sync.callRPC("registerService", ["spaceify.org/services/bigscreen"], self);
-
-			// Application initialialized itself successfully.
-			console.log(Config.CLIENT_APPLICATION_INITIALIZED);
-		}
-		catch(err)
-		{
-			// Application failed to initialialize itself. Pass the error message to the core.
-			logger.error("{{" + (err.message ? err.message : "") + "}}");
-			console.log(Config.CLIENT_APPLICATION_UNINITIALIZED);
-
-			stop.sync();
-		}
-		finally
-		{
-			rpcCore.sync.close();
-		}
-	});
-
-	var stop = fibrous( function()
-	{
-		RPCS.sync.close();
-		RPCSS.sync.close();
-
-		httpServer.sync.close();
-		httpsServer.sync.close();
-	});
-
-	/************************
-	* MANAGE CONNECTIONS!!! *
-	************************/
-	var disconnectionListener = function(connection)
-	{
-		if(bigscreens[connection.id])
-			delete bigscreens[connection.id];
 	}
 
-	/***************************
-	* EXPOSED JSON-RPC METHODS *
-	***************************/
-
-	// Add a big screen to the connected big screens
-	self.bigScreenConnect = fibrous( function(bs_id)
+var onClientDisconnected = function(serviceObj)
 	{
-		var connection = arguments[arguments.length - 1];
-		bigscreens[connection.id] = {bs_id: bs_id, connection: connection, is_secure: connection.is_secure};
-	});
+	if(bigscreens[serviceObj.id])
+		delete bigscreens[serviceObj.id];
+	}
 
-	// Clients call this to show their content on big screen IFRAME(s).
-	self.loadContent = fibrous( function(url, bs_id, content_type)
+	// EXPOSED JSON-RPC METHODS -- -- -- -- -- -- -- -- -- -- //
+var bigScreenConnect = function(bs_id, rpcObj)
 	{
-		var count = -1;
+	bigscreens[rpcObj.id] = {bs_id: bs_id, is_secure: rpcObj.is_secure};	// Add big screen to the connected big screens
+	}
 
-		for(var id in bigscreens)												// Send content URL/type to big screens having the bs_id or all the big screens
+var loadContent = function(url, bs_id, content_type)
+	{ // Clients call this to show their content on the big screen IFRAME(s).
+	for(var id in bigscreens)												// Send content URL/type to big screens having the bs_id or all the big screens
 		{
-			if(bigscreens[id].bs_id == bs_id || !bs_id)
-				count += (!bigscreens[id].is_secure ? RPCS : RPCSS).sync.callRPC("loadContent", [url, content_type], self, null, id);
+		if(bigscreens[id].bs_id == bs_id || !bs_id)
+			(!bigscreens[id].is_secure ? bs_service : bs_service_secure).callRpc("loadContent", [url, content_type], self, null, id);
+		}
+	}
+
+var getBigScreenIds = function()
+	{ // Return a list of unique big screen ids.
+	var ids = [];
+	for(var id in bigscreens)
+		{
+		if(!ids.indexOf(bigscreens[id].bs_id))
+			ids.push(bigscreens[id].bs_id);
 		}
 
-		return count;	// * -1 "No big screens having the big screen id are available. ** >= 0 "Content web page is ready or loading."
-	});
+	return ids;
+	}
 
-	// Return a list of unique big screen ids.
-	self.getBigScreenIds = fibrous( function()
+	// IMPLEMENT start AND fail IN YOUR APPLICATION!!! -- -- -- -- -- -- -- -- -- -- //
+self.start = function()
 	{
-		var ids = [];
-		for(var id in bigscreens)
-		{
-			if(!ids.indexOf(bigscreens[id].bs_id))
-				ids.push(bigscreens[id].bs_id);
-		}
+	bs_service = spaceify.getProvidedService(bs_service_name);
+	bs_service_secure = spaceify.getProvidedServiceSecure(bs_service_name);
 
-		return ids;
-	});
+	spaceify.exposeRpcMethodProvided("loadContent", self, loadContent, bs_service_name);
+	spaceify.exposeRpcMethodProvided("getBigScreenIds", self, getBigScreenIds, bs_service_name);
+	spaceify.exposeRpcMethodProvided("bigScreenConnect", self, bigScreenConnect, bs_service_name);
+
+	spaceify.setConnectionListenersProvided(onClientConnected, bs_service_name);
+	spaceify.setDisconnectionListenersProvided(onClientDisconnected, bs_service_name);
+	}
+
+self.fail = function(err)
+	{
+	
+	}
+
+var stop = function()
+	{
+	spaceify.stop();
+	}
+
 }
 
-fibrous.run(function()
-	{
-	logger.setOptions({labels: logger.ERROR});
-
-	bigscreen = new BigScreen();
-	bigscreen.sync.start();
-	});
+var bigScreen = new BigScreen();
+spaceify.start(bigScreen, {webservers: {http: true, https: true}});
--- application/www/language.js
+++ application/www/language.js
@@ -5,6 +5,7 @@ function Language()
 	self.CONNECTED = "Connected to the big screen hub";
 	self.E_CONNECTION_LOST = "Connection to the big screen hub lost";
 	self.E_CONNECTION_FAILED = "Failed to connect to the big screen hub: ";
+	self.E_CONNECTION_UNINITIALIZED = "The connection to the bigscreen hub is uninitialized";
 }
 
 var language = new Language();
